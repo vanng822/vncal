e777e86dd31f8df96e25c0430a5e32e4db3a311b
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-05-04 16:02:11 +0200

    VNDate may be too complicated. Will return array of the date

--- tests/test_vncal.js
+++ tests/test_vncal.js
@@ -1,11 +1,10 @@
 var assert = require('assert');
 var vows = require('vows');
 
-vncal = require('../index.js');
+vncal = require('../index.js'), VNDate = vncal.VNDate;
 
 vows.describe('Test suite for calc').addBatch({
 	'today should return a Date': function() {
-		assert.ok(vncal.today() instanceof Date);
-		//console.log(vncal.today().toISOString())
+		assert.ok(vncal.today() instanceof VNDate);
 	}	
 }).export(module);
