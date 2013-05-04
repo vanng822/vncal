18ecb17e913248e8765fb5f5590124cae3fc1d81
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-05-04 15:42:40 +0200

    Adding today

--- tests/test_vncal.js
+++ tests/test_vncal.js
@@ -4,5 +4,8 @@ var vows = require('vows');
 vncal = require('../index.js');
 
 vows.describe('Test suite for calc').addBatch({
-	
+	'today should return a Date': function() {
+		assert.ok(vncal.today() instanceof Date);
+		//console.log(vncal.today().toISOString())
+	}	
 }).export(module);
